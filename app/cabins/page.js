6682a8dfaaecdae928f6e092e849f81a6dabf011
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-23 17:53:57 +1300

    Update component imports for consistency

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,7 @@
-import Counter from "../_components/Counter";
-
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
+export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-
-      <Counter />
     </div>
   );
 }
