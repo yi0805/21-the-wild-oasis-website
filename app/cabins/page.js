e9921a354db2225c172be33fcfa65d93ff9dd0ca
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-23 15:51:53 +1300

    add Logo and Navigation components; update layout and main page structure

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1>Cabins page</h1>;
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>THe Wild Oasis. Welcome to paradise.</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
