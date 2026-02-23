1bcffde50dcdb72539cf4a493711332b4bb4ae84
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-23 16:33:13 +1300

    add Counter component to cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,20 @@
-export default function Page() {
+import Counter from "./Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
       <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+
+      <Counter />
     </div>
   );
 }
