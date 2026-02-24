2b2999ab92491193499f948e4b94a4a54f2007c8
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-24 18:53:15 +1300

    Add filter component for cabin capacity and integrate with CabinList

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -3,7 +3,7 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
 
   const cabins = await getCabins();
@@ -12,9 +12,25 @@ async function CabinList() {
     return null;
   }
 
+  let filteredCabins;
+  if (filter === "all") {
+    filteredCabins = cabins;
+  }
+  if (filter === "small") {
+    filteredCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    filteredCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity < 8,
+    );
+  }
+  if (filter === "large") {
+    filteredCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
