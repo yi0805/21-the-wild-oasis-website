ae8a14cbe373f1f953103a6ec850c6f0ec6b0f5a
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-26 17:54:48 +1300

    Enhance profile update functionality by integrating form submission and updating guest data; include loading state for the update button

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,8 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { updateGuest } from "./data-service";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -8,4 +10,28 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateProfileAction(formData) {
+  const session = await auth();
+
+  if (!session) {
+    throw new Error("You must be logged in");
+  }
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error("Please provide a valid national ID");
+  }
+
+  const updatedGuest = {
+    nationalID,
+    nationality,
+    countryFlag,
+  };
+
+  await updateGuest(session.user.guestId, updatedGuest);
+  revalidatePath("/account/profile");
 }
