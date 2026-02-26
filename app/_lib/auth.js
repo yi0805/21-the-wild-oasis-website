37c92b2b3c72cf367fddfdf364816b7b2262ebca
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-26 15:36:16 +1300

    Add authorization callback to NextAuth configuration and create middleware for account routes

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,6 +8,11 @@ const authConfig = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
